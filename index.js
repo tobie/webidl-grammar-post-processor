ab3c8d0d2f0e067f9e728c30259e81070d358cbb
Tobie Langel <tobie@codespeaks.com>
2017-10-13 15:08:19 +0200

    Add pp-webidl binary.

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 "use strict";
 
-const process = require("./process");
+const postProcess = require("./post-process");
 const jsdom = require("jsdom");
-const fs = require("fs");
-const getStdin = require("get-stdin");
 
 module.exports = (inputString, options) => new Promise((resolve, reject) => {
     let dom = new jsdom.JSDOM(inputString);
     let document = dom.window.document;
     [].forEach.call(document.querySelectorAll("script.remove"), (element) => element.remove());
-    process(document);
+    postProcess(document);
     resolve(dom.serialize());
 });
-
-if (require.main === module) {
-    getStdin.buffer()
-        .then(module.exports)
-        .then(str => console.log(str))
-        .catch(err => {
-            console.error(err);
-            process.exit(1);
-        });
-}
